a863c79c622e53fe230dc6c5c753e2d86a4a9b7b
Geoffrey Fardeau <fardeau.geoffrey@gmail.com>
2023-09-28 17:55:55 +0200

    routes finished, select pokemon by id

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
 
+  pokemonList: Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null  = this.route.snapshot.paramMap.get('id');
+
+    if(pokemonId) {
+
+   this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+   
+  }else {
+      this.pokemon = undefined;
+    }
+
+    
+
+  }
+
+  goBack() {
+    this.router.navigate(['/'])
+  }
 }
